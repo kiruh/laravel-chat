917a26db98c0510893abf765b8cb31f524f55592
Kiril Nedelev <nedelev.kiril@gmail.com>
2020-04-23 17:56:48 +0300

    lesson 1

---
+++ resources/js/components/ContactList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="contact-list">
+    <ul>
+      <li v-for="contact in contacts" :key="contact.id">
+        <div class="avatar">
+          <img src="https://via.placeholder.com/150" :alt="contact.name" />
+        </div>
+        <div class="contact">
+          <p class="name">{{ contact.name }}</p>
+          <p class="email">{{ contact.email }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    contacts: {
+      type: Array,
+      default: []
+    }
+  }
+};
+</script>
